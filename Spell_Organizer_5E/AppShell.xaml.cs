35e37e34e92ecc7983564d4b5c459a5f4b9d5138
icknominious <icknominious@gmail.com>
2019-06-01 15:36:33 -0500

    implemented natabase, not yet preloaded

--- Spell_Organizer_5E/AppShell.xaml.cs
+++ Spell_Organizer_5E/AppShell.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,12 +12,14 @@ namespace Spell_Organizer_5E
 {
     public partial class AppShell : Shell
     {
-        Random rand = new Random();
+        //Random rand = new Random();
         Dictionary<string, Type> routes = new Dictionary<string, Type>();
         public Dictionary<string, Type> Routes { get { return routes; } }
 
         public ICommand HelpCommand => new Command<string>((url) => Device.OpenUri(new Uri(url)));
-        public ICommand RandomPageCommand => new Command(async () => await NavigateToRandomPageAsync());
+        //public ICommand RandomPageCommand => new Command(async () => await NavigateToRandomPageAsync());
+
+        static SpellDatabase database;
 
         public AppShell()
         {
@@ -24,6 +27,20 @@ namespace Spell_Organizer_5E
             RegisterRoutes();
             BindingContext = this;
         }
+
+
+        public static SpellDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new SpellDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Spells.db3"));
+                }
+                return database;
+            }
+        }
+
 
         void RegisterRoutes()
         {
@@ -39,34 +56,34 @@ namespace Spell_Organizer_5E
             }
         }
 
-        async Task NavigateToRandomPageAsync()
-        {
-            string destinationRoute = routes.ElementAt(rand.Next(0, routes.Count)).Key;
-            string animalName = null;
+        //async Task NavigateToRandomPageAsync()
+        //{
+        //    string destinationRoute = routes.ElementAt(rand.Next(0, routes.Count)).Key;
+        //    string animalName = null;
 
-            switch (destinationRoute)
-            {
-                case "monkeydetails":
-                    animalName = MonkeyData.Monkeys.ElementAt(rand.Next(0, MonkeyData.Monkeys.Count)).Name;
-                    break;
-                case "beardetails":
-                    animalName = BearData.Bears.ElementAt(rand.Next(0, BearData.Bears.Count)).Name;
-                    break;
-                case "catdetails":
-                    animalName = CatData.Cats.ElementAt(rand.Next(0, CatData.Cats.Count)).Name;
-                    break;
-                case "dogdetails":
-                    animalName = DogData.Dogs.ElementAt(rand.Next(0, DogData.Dogs.Count)).Name;
-                    break;
-                case "elephantdetails":
-                    animalName = ElephantData.Elephants.ElementAt(rand.Next(0, ElephantData.Elephants.Count)).Name;
-                    break;
-            }
+        //    switch (destinationRoute)
+        //    {
+        //        case "monkeydetails":
+        //            animalName = MonkeyData.Monkeys.ElementAt(rand.Next(0, MonkeyData.Monkeys.Count)).Name;
+        //            break;
+        //        case "beardetails":
+        //            animalName = BearData.Bears.ElementAt(rand.Next(0, BearData.Bears.Count)).Name;
+        //            break;
+        //        case "catdetails":
+        //            animalName = CatData.Cats.ElementAt(rand.Next(0, CatData.Cats.Count)).Name;
+        //            break;
+        //        case "dogdetails":
+        //            animalName = DogData.Dogs.ElementAt(rand.Next(0, DogData.Dogs.Count)).Name;
+        //            break;
+        //        case "elephantdetails":
+        //            animalName = ElephantData.Elephants.ElementAt(rand.Next(0, ElephantData.Elephants.Count)).Name;
+        //            break;
+        //    }
 
-            ShellNavigationState state = Shell.Current.CurrentState;
-            await Shell.Current.GoToAsync($"{state.Location}/{destinationRoute}?name={animalName}");
-            Shell.Current.FlyoutIsPresented = false;
-        }
+        //    ShellNavigationState state = Shell.Current.CurrentState;
+        //    await Shell.Current.GoToAsync($"{state.Location}/{destinationRoute}?name={animalName}");
+        //    Shell.Current.FlyoutIsPresented = false;
+        //}
 
         void OnNavigating(object sender, ShellNavigatingEventArgs e)
         {
